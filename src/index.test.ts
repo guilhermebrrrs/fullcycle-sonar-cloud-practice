1250d16620bd2313c9120efe01114af52ab33de2
Guilherme Barra Orsi <guilhermebrrrs@hotmail.com>
2024-08-21 14:24:59 -0300

    fix: adjust tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -9,7 +9,7 @@ import {
 } from "./functions";
 import { primes } from "./utils";
 
-test("add function", () => {
+/* test("add function", () => {
   expect(add(1000, 1)).toBe(1001);
 });
 
@@ -17,7 +17,7 @@ test("divide function", () => {
   expect(divide(25, 2)).toBe(12.5);
 });
 
-/* test("factorial function", () => {
+test("factorial function", () => {
   expect(factorial(12)).toBe(479001600);
 });
 
