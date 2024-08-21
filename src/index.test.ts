9f8ce60a69186f9e5421a23776667ebea17d4325
Guilherme Barra Orsi <guilhermebrrrs@hotmail.com>
2024-08-21 14:05:16 -0300

    feat: improve tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,12 @@
-import { add, divide, factorial, isPrime, multiply, power, subtract } from "./functions";
+import {
+  add,
+  divide,
+  factorial,
+  isPrime,
+  multiply,
+  power,
+  subtract,
+} from "./functions";
 import { primes } from "./utils";
 
 test("add function", () => {
@@ -13,10 +21,14 @@ test("factorial function", () => {
   expect(factorial(12)).toBe(479001600);
 });
 
-test("power function", () => {
-  expect(power(2, 10)).toBe(1024);
-});
-
 test(`isPrime function`, () => {
   primes.forEach((number) => expect(isPrime(number)).toBeTruthy);
+});
+
+test("multiply function", () => {
+  expect(multiply(8, 2)).toBe(16);
+});
+
+test("power function", () => {
+  expect(power(2, 10)).toBe(1024);
 });
